b80a3a284df7e6eefbfd7a39a4caf4dbf617e30c
Matheus Casa Nova <mcnl@iMac-de-prospecmol.local>
2018-06-01 11:34:16 -0300

    Version 0.0.4 - Sign Up function is receiveing data from html, not gender yet

--- js/backend.js
+++ js/backend.js
@@ -6,3 +6,22 @@ function irparacadastro(){
 	console.log("Indo para cadastro!");
 	window.location.href = "cadastro.html";
 }
+
+function cadastrar(){
+	var nome = document.getElementById("name").value;
+	var cpf = document.getElementById("cpf").value;
+	var email = document.getElementById("email").value;
+	var idade = document.getElementById("idade").value;
+	var sexo;
+	
+
+	var cluster = document.getElementById("cluster").value;
+
+	window.alert(nome);
+	window.alert(cpf);
+	window.alert(email);
+	window.alert(idade);
+	window.alert(sexo);
+	window.alert(cluster);
+
+}
